4ca6d35e351103a5ee9ba19e096a6174f16b0738
splash <splashs089@gmail.com>
2022-08-31 23:22:35 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Anes Khalifa",
+      bio: "Learning ReactJS",
+      profession: "Student",
+      imgSrc: "/ph.jpg",
+    },
+    show: false,
+  };
+  HandleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return (
+      <>
+        <button onClick={this.HandleShow}>Show Profile</button>
+        {this.state.show ? (
+          <div className="cc">
+            <div className="card">
+              <img src={this.state.person.imgSrc} alt="Avatar" />
+              <div className="container">
+                <h4>
+                  <b>
+                    {this.state.person.fullName} -{" "}
+                    {this.state.person.profession}{" "}
+                  </b>
+                </h4>
+                <p>{this.state.person.bio}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <h2>Click To Show the profile</h2>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
